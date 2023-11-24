c61010330db104d3fc963d17fd15d94aabc46dc5
Blasidelgado <blasidelgado@gmail.com>
2023-11-24 11:11:19 -0300

    Add UserProfile model description field

--- travel/models.py
+++ travel/models.py
@@ -6,12 +6,26 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     license = models.CharField(max_length=30, blank=True, null=True)
     car = models.OneToOneField('Car', blank=True, null=True, on_delete=models.SET_NULL)
+    description = models.TextField(max_length=200, blank=True, null=True)
 
     def has_license(self):
         return self.license is not None
     
     def has_car(self):
         return self.car is not None
+
+    def to_json(self):
+        return {
+            'username': self.user.username,
+            'first_name': self.user.first_name,
+            'last_name': self.user.last_name,
+            'license': self.license,
+            'car': {
+                'plate_number': self.car.plate_number if self.car else None,
+                'brand': self.car.brand if self.car else None,
+                'model': self.car.model if self.car else None,
+            } if self.has_car() else None
+        }
 
 
 class Car(models.Model):
