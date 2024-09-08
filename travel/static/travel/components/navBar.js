d60c2a983b9bc727538995fb9125cf7c0c9e7cde
Blasidelgado <blasidelgado@gmail.com>
2024-09-08 19:46:06 -0300

    refactor: header navbar to match routing

--- travel/static/travel/components/navBar.js
+++ travel/static/travel/components/navBar.js
@@ -7,43 +7,37 @@ export default function navBar(sessionStatus) {
     navBar.innerHTML = sessionStatus ?
     `<div class="container-fluid flex-nowrap">
         <div class="container img-wrapper">
-            <img class="navbar-brand page-btn" data-page="home" src="/media/logo.jpg" alt="logo" role="button" width="85" height="70">
+            <img class="navbar-brand page-btn" data-page="/" src="/media/logo.jpg" alt="logo" role="button" width="85" height="70">
         </div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse flex-grow-1 w-75 z-1" id="navbarNav">
             <ul class="navbar-nav d-lg-flex justify-content-evenly w-100">
-                <li class="nav-item page-btn" data-page="travel">Travel now</li>
-                <li class="nav-item page-btn" data-page="journeys">All journeys</li>
-                <li class="nav-item page-btn" data-page="userJourneys">My journeys</li>
-                <li class="nav-item page-btn" data-page="profile">My profile</li>
-                <li class="nav-item page-btn" data-page="logout">Logout</li>
+                <li class="nav-item page-btn" data-page="/travel">Travel now</li>
+                <li class="nav-item page-btn" data-page="/journeys">All journeys</li>
+                <li class="nav-item page-btn" data-page="/my-journeys">My journeys</li>
+                <li class="nav-item page-btn" data-page="/profile">My profile</li>
+                <li class="nav-item page-btn" data-page="/logout">Logout</li>
             </ul>
         </div>
     </div>` 
     :
     `<div class="container-fluid flex-nowrap">
         <div class="container w-25">
-            <img class="navbar-brand page-btn" data-page="home" src="/media/logo.jpg" alt="logo" role="button" width="85" height="70">        </div>
+            <img class="navbar-brand page-btn" data-page="/" src="/media/logo.jpg" alt="logo" role="button" width="85" height="70">        </div>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </div>
         </button>
         <div class="collapse navbar-collapse w-75 z-1" id="navbarNav">
             <ul class="navbar-nav d-lg-flex justify-content-evenly w-100">
-                <li class="nav-item page-btn" data-page="journeys">All journeys</li>
-                <li class="nav-item page-btn" data-page="login">Login</li>
-                <li class="nav-item page-btn" data-page="register">Register</li>
+                <li class="nav-item page-btn" data-page="/journeys">All journeys</li>
+                <li class="nav-item page-btn" data-page="/login">Login</li>
+                <li class="nav-item page-btn" data-page="/register">Register</li>
             </ul>
         </div>
     </div>`;
-
-    navBar.querySelectorAll(".page-btn").forEach(btn => {
-        btn.addEventListener("click", () => {
-            changeAppState(btn.dataset.page, sessionStorage.getItem('username'), 1);
-        })
-    });
 
     return navBar;
 }
